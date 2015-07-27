c547afd457f406996de8e6bde7ffc58370ddd683
Vytautas Šaltenis <vytas@rtfb.lt>
2015-07-27 11:54:35 +0300

    Add WebView to IntentReceiver and show the received URL in it

--- app/src/main/java/lt/rtfb/rdr/IntentReceiver.java
+++ app/src/main/java/lt/rtfb/rdr/IntentReceiver.java
@@ -1,17 +1,27 @@
 package lt.rtfb.rdr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 
 public class IntentReceiver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Let's display the progress in the activity title bar, like the
+        // browser app does.
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ir);
         Intent i = getIntent();
         String action = i.getAction();
         String type = i.getType();
@@ -19,6 +29,25 @@ public class IntentReceiver extends AppCompatActivity {
         android.util.Log.i("zzz", action);
         android.util.Log.i("zzz", type);
         android.util.Log.i("zzz", sharedText);
+
+        final WebView webview = (WebView) findViewById(R.id.main_webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webview.loadUrl(sharedText);
     }
 
     @Override
---
+++ app/src/main/java/lt/rtfb/rdr/IntentReceiverActivityFragment.java
@@ -0,0 +1,23 @@
+package lt.rtfb.rdr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class IntentReceiverActivityFragment extends Fragment {
+
+    public IntentReceiverActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ir, container, false);
+    }
+}
