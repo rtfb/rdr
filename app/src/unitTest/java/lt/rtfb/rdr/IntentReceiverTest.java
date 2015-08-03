8e271a6bcad0b85a57ce6678b18c7ea62103bb46
Vytautas Šaltenis <vytas@rtfb.lt>
2015-08-03 18:45:54 +0300

    Add dummy unit test

---
+++ app/src/unitTest/java/lt/rtfb/rdr/IntentReceiverTest.java
@@ -0,0 +1,17 @@
+package lt.rtfb.rdr;
+
+import junit.framework.TestCase;
+
+public class IntentReceiverTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testFoo() {
+        assertTrue(true);
+    }
+}
